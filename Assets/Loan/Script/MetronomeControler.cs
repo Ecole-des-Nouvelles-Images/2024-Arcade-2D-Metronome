0b06ade46024d67df672a0bbd72492d7f02ac34a
Ritail <loan.jourdan@gmail.com>
2024-11-21 17:04:50 +0100

    Add_Scroll_Note

--- Assets/Loan/Script/MetronomeControler.cs
+++ Assets/Loan/Script/MetronomeControler.cs
@@ -2,29 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MetronomeControler : MonoBehaviour
 {
     private InputSysteme _inputSysteme;
-    private Renderer _renderer;
+    private Image _image;
 
     private void Start()
     {
         _inputSysteme = GetComponent<InputSysteme>();
-        _renderer = GetComponent<Renderer>();
+        _image = GetComponent<Image>();
     }
 
     private void Update()
     {
         if (_inputSysteme.Active == 1)
         {
-            _renderer.material.color = Color.red;
+            _image.color = new Color(1, 0, 0, 0.5f);
             Invoke(nameof(Reset),2f);
         }
     }
 
     private void Reset()
     {
-        _renderer.material.color = Color.white;
+        _image.color = new Color(0, 1,0, 0.5f);
     }
 }
