c5161820c1a3df3de0486cdf3dc2ffcc4de5704d
Ritail <loan.jourdan@gmail.com>
2024-12-12 22:03:34 +0100

    Add_MapFini_ScriptSliderSons_Fait

--- Assets/Loan/Script/Musique/MusicManager.cs
+++ Assets/Loan/Script/Musique/MusicManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
@@ -10,6 +12,10 @@ public class MusicManager : MonoBehaviour
 
     [SerializeField] private AudioClip _defaultMusic;
     [SerializeField] private AudioClip _gameMusic;
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _masterSlider;
+    [SerializeField] private Slider _musiqueSlider;
+    [SerializeField] private Slider _sfxSlider;
     
 
     private void Awake()
@@ -30,6 +36,44 @@ public class MusicManager : MonoBehaviour
         {
             PlayMusic(_defaultMusic);
         }
+    }
+
+    private void Start()
+    {
+        _masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+        _musiqueSlider.value = PlayerPrefs.GetFloat("MusiqueVolume", 0.75f);
+        _sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+
+        ApplyVolumeSetting();
+        
+        _masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        _musiqueSlider.onValueChanged.AddListener(SetMusicVolume);
+        _sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+    
+    private void ApplyVolumeSetting()
+    {
+        SetMasterVolume(_masterSlider.value);
+        SetMusicVolume(_musiqueSlider.value);
+        SetSFXVolume(_sfxSlider.value);
+    }
+    
+    private void SetMasterVolume(float volume)
+    {
+        _audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+    }
+    
+    private void SetMusicVolume(float volume)
+    {
+        _audioMixer.SetFloat("MusiqueVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusiqueVolume", volume);
+    }
+
+    private void SetSFXVolume(float volume)
+    {
+        _audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
     public void StopMusic()
