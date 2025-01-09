64d15bcddc30da881dbc15c576b46501056258f7
Ritail <loan.jourdan@gmail.com>
2025-01-09 14:58:05 +0100

    Add_fondu_SceneENSI

--- Assets/Loan/Script/ENSIScene.cs
+++ Assets/Loan/Script/ENSIScene.cs
@@ -1,16 +1,49 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ENSIScene : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] private float _speed = 0.38f;
+    [SerializeField] private GameObject _slashScreen;
+    private Image _image;
+    private bool _isActive;
+
+    private void Awake()
     {
-        StartCoroutine(OpenMainMenu());
+        _image = _slashScreen.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        float Alpha = _image.color.a;
+        if (Alpha <= 1 && _isActive == false)
+        {
+            var Incress = Alpha += _speed * Time.deltaTime;
+        
+            _image.color = new Color(0,0,0,Incress);
+            transform.DOScale(1, 2f);
+            if (Alpha >= 1)
+            {
+                _isActive = true;
+            }
+        }
+        if (_isActive)
+        {
+            var Incress = Alpha -= _speed * Time.deltaTime;
+        
+            _image.color = new Color(0,0,0,Incress);
+            if (Alpha <= 0)
+            {
+                StartCoroutine(OpenMainMenu());
+            }
+        }
     }
 
     private System.Collections.IEnumerator OpenMainMenu()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene("MenuScene");
     }
 }
