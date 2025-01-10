f17c6e4b208c9ca9641b0193b54f1e3fe5050959
Ritail <loan.jourdan@gmail.com>
2025-01-10 11:10:47 +0100

    Update_Trapp_Cle

--- Assets/Loan/Script/Piege/Cle.cs
+++ Assets/Loan/Script/Piege/Cle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ using UnityEngine;
 public class Cle : PiegeData
 {
     public float ExplosionRadius = 3f;
-    private float ExplosionForce = 1200f;
+    private float ExplosionForce = 25f;
 
     private void OnEnable()
     {
@@ -23,12 +24,15 @@ public class Cle : PiegeData
             RunnersControler runner = hit.GetComponent<RunnersControler>();
             if (runner != null)
             {
-               Rigidbody2D rb = runner.GetComponent<Rigidbody2D>();
-               if (rb != null)
-               {
-                   Vector2 forceDirection = (runner.transform.position - (Vector3)position).normalized;
-                   rb.AddForce(forceDirection * ExplosionForce);
-               }
+                Rigidbody2D rb = runner.GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    Vector2 forceDirection = (runner.transform.position - (Vector3)position).normalized;
+                    forceDirection = new Vector2(forceDirection.x, 0.7f).normalized;
+                    
+                    
+                    rb.AddForce(forceDirection * ExplosionForce, ForceMode2D.Impulse);
+                }
             }
         }
     }
